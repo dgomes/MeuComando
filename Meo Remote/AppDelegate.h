fce29f4cd7425834267b62c81ee385c16e883779
Diogo Gomes <diogogomes@gmail.com>
2012-01-23 01:10:11 +0000

    Initial Commit

---
+++ Meo Remote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Meo Remote
+//
+//  Created by Diogo Gomes on 23/01/12.
+//  Copyright (c) 2012 Universidade de Aveiro. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Meo Remote/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  Meo Remote
+//
+//  Created by Diogo Gomes on 23/01/12.
+//  Copyright (c) 2012 Universidade de Aveiro. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
